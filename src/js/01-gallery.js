1dee481d5fe420587483770a7facf533964b17d0
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-26 23:54:42 +0300

    Interim Commit 26072022 (parcel installation))

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+// console.log(galleryItems);
+
+
+// Gallery Construction
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryToInclude = galleryItems.map(item => `
+    <a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+    </a>    
+`).join("");
+
+galleryContainer.innerHTML = galleryToInclude;
+
+const galleryLinkElement = document.querySelectorAll(".gallery__link");
+
+galleryLinkElement.forEach(() => {
+    addEventListener("click", (event) => event.preventDefault());
+});
+
+// Simple Light Box initialisation 
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
