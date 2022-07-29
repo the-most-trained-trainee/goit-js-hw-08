e94f6b48a9a9f8512393f197a56777c46343f89e
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-30 01:15:15 +0300

    Interim commit 30072022 (task 3/3))

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from "@vimeo/player";
-import throttle from "lodash.throttle"
+import throttle from "lodash.throttle";
 
 const heroPlayer = new Player(document.querySelector("#vimeo-player"));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from "lodash.throttle";
 
+const loginForm = document.querySelector(".feedback-form");
+
+
+
+loginForm.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+
+    // if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
+    //     alert("Please, fill all the fields");
+    // } else {
+    //     const formData = new FormData(event.currentTarget);
+    //     let formOutPut = {};
+
+    //     formData.forEach((value, name) => {
+    //         formOutPut[name] = value;
+    //     });
+    //     event.currentTarget.reset();
+    //     console.log(formOutPut);
+    // }
+}
