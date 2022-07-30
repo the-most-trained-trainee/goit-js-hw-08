100583db8d10adaade08e5e1b54702769941f3ce
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-07-30 15:36:24 +0300

    Final Commit 30.07.2022 (Task 3/3)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,34 @@
 import throttle from "lodash.throttle";
 
 const loginForm = document.querySelector(".feedback-form");
+const loginFormEmail = document.querySelector(".feedback-form input");
+const loginFormText = document.querySelector(".feedback-form textarea");
 
+if (localStorage.getItem("feedback-form-state") !== null && localStorage.getItem("feedback-form-state") !== "") {
+    loginFormText.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+    loginFormEmail.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
+}
 
+function formDataReceipt() {
+    const formData = new FormData(loginForm);
+    let formOutPut = {};
+    formData.forEach((value, name) => {
+        formOutPut[name] = value;
+    });
+    return formOutPut;
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.message.value === "") {
+        alert("Please, fill all the fields");
+    } else {
+        console.log(formDataReceipt(event));
+        event.currentTarget.reset();
+        localStorage.setItem("feedback-form-state", "")
+    }
+}
 
 loginForm.addEventListener("submit", onSubmit);
 
-function onSubmit(event) {
-
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-
-
-    // if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
-    //     alert("Please, fill all the fields");
-    // } else {
-    //     const formData = new FormData(event.currentTarget);
-    //     let formOutPut = {};
-
-    //     formData.forEach((value, name) => {
-    //         formOutPut[name] = value;
-    //     });
-    //     event.currentTarget.reset();
-    //     console.log(formOutPut);
-    // }
-}
+loginForm.addEventListener("input", throttle(formInput => localStorage.setItem("feedback-form-state", JSON.stringify(formDataReceipt(formInput))), 500));
